683f74921eff8f71cc2bee73e0afe4bc9eb1b45c
Lorenzo Dentis <lory9894@gmail.com>
2023-07-07 17:29:05 +0200

    initial commit

---
+++ implementazione/src/Approximations.java
@@ -0,0 +1,14 @@
+public class Approximations {
+
+    public static void one_third_approximation() {
+        CutSet A = new CutSet();
+
+    }
+
+    public static void half_approximation() {
+    }
+    public static void main(String[] args) {
+        one_third_approximation();
+        half_approximation();
+    }
+}
---
+++ implementazione/src/CutSet.java
@@ -0,0 +1,11 @@
+public class CutSet {
+//https://github.com/bunelr/sfm-min/tree/master/min-cut-pb
+
+
+    public CutSet() {
+    }
+    public float evaluate() {
+        //todo:capire qual'è la funzione che dato un set restituisce la valutazione (cioè la funione f)
+        return 0;
+    }
+}
