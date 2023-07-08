941bef9f25555ea71c634224d983d3a3576a8ed3
Lorenzo Dentis <lory9894@gmail.com>
2023-07-08 19:15:19 +0200

    removed weights

--- implementazione/src/Grap.java
+++ implementazione/src/Grap.java
@@ -6,11 +6,10 @@ import java.util.*;
  * Edge: Edge class for the adjacency list, public so classes outside Graph can access it if needed
  */
 class Edge {
-    int src, dest, weight;
-    Edge(int src, int dest, int weight) {
+    int src, dest;
+    Edge(int src, int dest) {
             this.src = src;
             this.dest = dest;
-            this.weight = weight;
         }
 }
 // Graph class
@@ -22,10 +21,9 @@ class Graph {
      * Node: Node class for the adjacency list, private so only Graph can access it
      */
     private static class Node {
-        int value, weight;
-        Node(int value, int weight)  {
+        int value;
+        Node(int value)  {
             this.value = value;
-            this.weight = weight;
         }
     }
 
@@ -42,8 +40,7 @@ class Graph {
                 String[] tokens = line.split(" ");
                 int src = Integer.parseInt(tokens[0]);
                 int dest = Integer.parseInt(tokens[1]);
-                int weight = Integer.parseInt(tokens[2]);
-                edges.add(new Edge(src, dest, weight));
+                edges.add(new Edge(src, dest));
             }
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -82,7 +79,7 @@ class Graph {
             if (!adj_list.containsKey(e.src))
                 adj_list.put(e.src, new ArrayList<>());
             // add dest to src's list
-            adj_list.get(e.src).add(new Node(e.dest, e.weight));
+            adj_list.get(e.src).add(new Node(e.dest));
     }
 
     /***
@@ -90,7 +87,7 @@ class Graph {
      */
     public void removeEdge(Edge e) {
         // remove dest from src's list
-        adj_list.get(e.src).remove(new Node(e.dest, e.weight));
+        adj_list.get(e.src).remove(new Node(e.dest));
     }
 
     /***
@@ -113,19 +110,11 @@ class Graph {
         List<Edge> edges = new ArrayList<>();
         for (int src_vertex : adj_list.keySet()) {
             for (Node edge : adj_list.get(src_vertex)) {
-                edges.add(new Edge(src_vertex, edge.value, edge.weight));
+                edges.add(new Edge(src_vertex, edge.value));
             }
         }
         return edges;
     }
-
-    /***
-     * Equals: Checks if two graphs are equal
-     */
-    public boolean equals(Graph g) {
-        return this.adj_list.equals(g.adj_list); //todo: ma sarà uguaglianza di riferimento o di valori?
-    }
-
     /***
      * printGraph: Prints the graph, debugging purposes
      * @param graph
@@ -137,7 +126,7 @@ class Graph {
             System.out.print("|" + src_vertex + "| => ");
             //traverse through the adjacency list and print the edges
             for (Node edge : graph.adj_list.get(src_vertex)) {
-                System.out.print("[" + edge.value + " (" + edge.weight + ")] -> ");
+                System.out.print("[" + edge.value + "] -> ");
             }
             System.out.println("/");
         }
