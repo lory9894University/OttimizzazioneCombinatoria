c345a32d5335e91c8e888c387b22e7b6b0ec0d81
Lorenzo Dentis <lory9894@gmail.com>
2023-07-08 11:45:04 +0200

    Done writing algorithm 1/3 and 1/2

--- implementazione/src/CutSet.java
+++ implementazione/src/CutSet.java
@@ -8,17 +8,28 @@ public class CutSet {
     public CutSet() {
         g = new Graph();
     }
-    public void addElement(Edge e) {
+    private void addElement(Edge e) {
         g.addEdge(e);
     }
-    public void removeElement(Edge e) {
+    private void removeElement(Edge e) {
         g.removeEdge(e);
     }
 
+    public CutSet unionElem(Edge e) {
+        CutSet result = new CutSet();
+        result.addElement(e);
+        return result;
+    }
+    public CutSet setMinusElem(Edge e) {
+        CutSet result = new CutSet();
+        result.removeElement(e);
+        return result;
+    }
     public boolean equals(CutSet c) {
         return g.equals(c.g);
     }
-    public float evaluate() {
+
+    public static double evaluate(CutSet c) {
         //todo:capire qual'è la funzione che dato un set restituisce la valutazione (cioè la funione f)
         return 0;
     }
