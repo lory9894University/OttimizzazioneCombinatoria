781f473ad0fb82c214a066c9ba95ec2bd1fe8f59
Lorenzo Dentis <lory9894@gmail.com>
2023-07-08 11:45:04 +0200

    Done writing algorithm 1/3 and 1/2

--- implementazione/src/Approximations.java
+++ implementazione/src/Approximations.java
@@ -1,11 +1,20 @@
+
 public class Approximations {
 
     public static void one_third_approximation(Graph g) {
         CutSet A = new CutSet();
         CutSet B = new CutSet(g);
 
-        while (!A.equals(B)){
+        //For each element xi ∈ X
+        for (Edge e : g.getEdges()){
+            double ai = CutSet.evaluate(A.unionElem(e))-CutSet.evaluate(A);
+            double bi = CutSet.evaluate(B.unionElem(e))-CutSet.evaluate(B);
 
+            if (ai >= bi) {
+                A = A.unionElem(e);
+            } else {
+                B = B.setMinusElem(e);
+            }
         }
 
     }
@@ -14,7 +23,23 @@ public class Approximations {
         CutSet A = new CutSet();
         CutSet B = new CutSet(g);
 
-        //todo: non ho capito la condizione di terminazione
+        //For each element xi ∈ X
+         for (Edge e : g.getEdges()){
+            double ai = CutSet.evaluate(A.unionElem(e))-CutSet.evaluate(A);
+            double bi = CutSet.evaluate(B.unionElem(e))-CutSet.evaluate(B);
+
+            ai = Math.max(ai, 0);
+            bi = Math.max(bi, 0);
+            double decisor = Math.random();
+
+            if (decisor >= ai /(ai + bi)) { //todo: ma se faccio 0/0 + bi, java espolde?
+                A = A.unionElem(e);
+            } else {
+                B = B.setMinusElem(e);
+            }
+        }
+
+
     }
     public static void main(String[] args) {
        Graph g = new Graph("edges_test.txt");
