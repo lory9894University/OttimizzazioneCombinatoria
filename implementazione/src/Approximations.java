459d570de88157d1885388fc07219f0ff80e7a00
Lorenzo Dentis <lory9894@gmail.com>
2023-07-08 11:01:02 +0200

    Started writing algorithm 1/3 and 1/2

--- implementazione/src/Approximations.java
+++ implementazione/src/Approximations.java
@@ -1,11 +1,20 @@
 public class Approximations {
 
-    public static void one_third_approximation() {
+    public static void one_third_approximation(Graph g) {
         CutSet A = new CutSet();
+        CutSet B = new CutSet(g);
+
+        while (!A.equals(B)){
+
+        }
 
     }
 
-    public static void half_approximation() {
+    public static void half_approximation(Graph g) {
+        CutSet A = new CutSet();
+        CutSet B = new CutSet(g);
+
+        //todo: non ho capito la condizione di terminazione
     }
     public static void main(String[] args) {
        Graph g = new Graph("edges_test.txt");
