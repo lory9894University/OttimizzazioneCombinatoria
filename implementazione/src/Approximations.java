5bd3963df6940ea3fafe496a77320419c114b7af
Lorenzo Dentis <lory9894@gmail.com>
2023-07-10 13:49:24 +0200

    fixed an error in half_approximation

--- implementazione/src/Approximations.java
+++ implementazione/src/Approximations.java
@@ -1,4 +1,3 @@
-
 public class Approximations {
 
     public static void one_third_approximation(Graph g) {
@@ -6,9 +5,9 @@ public class Approximations {
         CutSet B = new CutSet(g);
 
         //For each element xi ∈ X
-        for (Edge e : g.getEdges()){
-            double ai = CutSet.evaluate(A.unionElem(e))-CutSet.evaluate(A);
-            double bi = CutSet.evaluate(B.setMinusElem(e))-CutSet.evaluate(B);
+        for (Edge e : g.getEdges()) {
+            double ai = CutSet.evaluate(A.unionElem(e)) - CutSet.evaluate(A);
+            double bi = CutSet.evaluate(B.setMinusElem(e)) - CutSet.evaluate(B);
 
             if (ai >= bi) {
                 A = A.unionElem(e);
@@ -25,25 +24,28 @@ public class Approximations {
         CutSet B = new CutSet(g);
 
         //For each element xi ∈ X
-         for (Edge e : g.getEdges()){
-            double ai = CutSet.evaluate(A.unionElem(e))-CutSet.evaluate(A);
-            double bi = CutSet.evaluate(B.setMinusElem(e))-CutSet.evaluate(B);
+        for (Edge e : g.getEdges()) {
+            double ai = CutSet.evaluate(A.unionElem(e)) - CutSet.evaluate(A);
+            double bi = CutSet.evaluate(B.setMinusElem(e)) - CutSet.evaluate(B);
 
             ai = Math.max(ai, 0);
             bi = Math.max(bi, 0);
             double decisor = Math.random();
 
-            if (decisor >= ai /(ai + bi)) {
-                A = A.unionElem(e);
-            } else {
+            if (decisor >= ai / (ai + bi)) {
                 B = B.setMinusElem(e);
+            } else {
+                A = A.unionElem(e);
             }
         }
-
+        Graph.printGraph(A.g);
+        Graph.printGraph(B.g);
 
     }
+
     public static void main(String[] args) {
-       Graph g = new Graph("book_test.txt");
-       one_third_approximation(g);
+        Graph g = new Graph("edges_test.txt");
+        one_third_approximation(g);
+        half_approximation(g);
     }
 }
