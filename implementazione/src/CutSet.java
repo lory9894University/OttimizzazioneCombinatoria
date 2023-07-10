4df6429f33fdadf6552fa414c5c186f9c19fa9eb
Lorenzo Dentis <lory9894@gmail.com>
2023-07-10 13:52:12 +0200

    rewrote evaluation function as in book

--- implementazione/src/CutSet.java
+++ implementazione/src/CutSet.java
@@ -1,18 +1,34 @@
 import java.util.ArrayList;
 
 public class CutSet {
-//https://github.com/bunelr/sfm-min/tree/master/min-cut-pb
+    //https://github.com/bunelr/sfm-min/tree/master/min-cut-pb
     Graph g;
 
     public CutSet(Graph g) {
         this.g = new Graph(g);
     }
+
     public CutSet() {
         g = new Graph();
     }
+
+    public static double evaluate(CutSet c) {
+        //For each subset A ⊆ V , ve define f (A) to be the number of edges running from A to V \A.
+        ArrayList<Edge> edges = new ArrayList<>(c.g.getEdges());
+        int crossingEdges = 0;
+        for (Edge edge : edges) {
+            //for each edge check if it's a cutting edge (arco di taglio)
+            if (c.g.adj_list.containsKey(edge.src) && !c.g.adj_list.containsKey(edge.dest)) {
+                crossingEdges += 1;
+            }
+        }
+        return crossingEdges;
+    }
+
     private void addElement(Edge e) {
         g.addEdge(e);
     }
+
     private void removeElement(Edge e) {
         g.removeEdge(e);
     }
@@ -22,25 +38,14 @@ public class CutSet {
         result.addElement(e);
         return result;
     }
+
     public CutSet setMinusElem(Edge e) {
         CutSet result = new CutSet(g);
         result.removeElement(e);
         return result;
     }
+
     public boolean equals(CutSet c) {
         return g.equals(c.g);
-    }
-
-    public static double evaluate(CutSet c) {
-        //For each subset A ⊆ V , ve define f (A) to be the number of edges running from A to V \A.
-        ArrayList<Edge> edges = new ArrayList<>(c.g.getEdges());
-        int crossingEdges = 0;
-        for (Edge edge : edges){
-            //for each edge check if it's a cutting edge (arco di taglio)
-            if (c.g.adj_list.containsKey(edge.src) && !c.g.adj_list.containsKey(edge.dest)) {
-                crossingEdges+= edge.weight;
-            }
-        }
-        return crossingEdges;
     }
 }
