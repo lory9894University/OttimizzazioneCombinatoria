9b7533931fdfc4b6a8e5e67251342864c8f57d1a
Lorenzo Dentis <lory9894@gmail.com>
2023-07-15 19:37:51 +0200

    Renamed variable for clarity

--- implementazione/src/CutSet.java
+++ implementazione/src/CutSet.java
@@ -32,14 +32,14 @@ public class CutSet {
     public static double evaluate(CutSet c) {
         //For each subset A ⊆ V , ve define f (A) to be the number of edges running from A to V \A.
         ArrayList<Edge> edges = new ArrayList<>(c.g.getEdges());
-        int crossingEdges = 0;
+        int crossingEdgesValue = 0;
         for (Edge edge : edges) {
             //for each edge check if it's a cutting edge (arco di taglio)
             if (c.g.adj_list.containsKey(edge.src) && !c.g.adj_list.containsKey(edge.dest)) {
-                crossingEdges += edge.weight;
+                crossingEdgesValue += edge.weight;
             }
         }
-        return crossingEdges;
+        return crossingEdgesValue;
 
         //in ogni caso questo definisce un max cut, non un min cut
     }
