41202aaae47eef0df2d81cb0bf730504ba8f7c3a
Lorenzo Dentis <lory9894@gmail.com>
2023-07-16 10:06:12 +0200

    little change to Approximations. alghoritm print cut value and both are runned

--- implementazione/src/Approximations.java
+++ implementazione/src/Approximations.java
@@ -44,11 +44,12 @@ public class Approximations {
         Graph.printGraph(A.g);
         Graph.printGraph(B.g);
 
+        System.out.println("Cut value (A): " + CutSet.evaluate(A) + "\nCut value (B): " + CutSet.evaluate(B));
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph("test1.txt");
+        Graph g = new Graph("test4.txt");
         one_third_approximation(g);
-
+        half_approximation(g);
     }
 }
